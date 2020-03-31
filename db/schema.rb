299f870021dfc0800225adbabd7418759ba74bb7
kz-oita <taoi.kizuka@gmail.com>
2020-03-31 21:46:10 +0900

    unicornの設定

--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,10 @@ ActiveRecord::Schema.define(version: 2020_03_31_013518) do
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.string "text", null: false
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.string "image"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "taggings", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -56,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_013518) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
