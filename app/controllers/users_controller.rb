544b4d8e4f7dca269f1c592f825f89d8ca747035
kz-oita <taoi.kizuka@gmail.com>
2020-04-09 09:58:28 +0900

    プロフィール編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.order("created_at DESC").page(params[:page]).per(4)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :image, :text, :diver_lank, :dive_number)
+  end
 end
