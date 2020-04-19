a0c5c77c1411022f00a01718688e2ef174e3f05f
kz-oita <taoi.kizuka@gmail.com>
2020-04-19 11:00:34 +0900

    turbolinks無効化

--- app/assets/javascripts/post_image.js
+++ app/assets/javascripts/post_image.js
@@ -1,38 +1,46 @@
-// $(function(){
-//   $fileField = $('#post_image')
- 
-//   // 選択された画像を取得し表示
-//   $($fileField).on('change', $fileField, function(e) {
-//     file = e.target.files[0]
-//     reader = new FileReader(),
-//     $preview = $("#preview");
- 
-//     reader.onload = (function(file) {
-//       return function(e) {
-//         $preview.empty();
-//         $preview.append($('<img>').attr({
-//           src: e.target.result,
-//           width: "100%",
-//           class: "preview",
-//           title: file.name
-//         }));
-//       };
-//     })(file);
-//     reader.readAsDataURL(file);
-//   });
-// });
 
-// $(function() {
-//   function readURL(input) {
-//     if (input.files && input.files[0]) {
-//       var reader = new FileReader();
-//       reader.onload = function (e) {
-//         $('#preview').attr('src', e.target.result);
+$(function(){
+  $fileField = $('#post_image')
+
+  // 選択された画像を取得し表示
+  $($fileField).on('change', $fileField, function(e) {
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $("#preview");
+
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+          src: e.target.result,
+          width: "100%",
+          class: "preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
+
+// $(function(){
+//   var cropper;
+//   var croppable = false;
+//   function initIconCrop(){
+//     cropper = new Cropper(crop_img, {
+//       dragMode: 'move',
+//       aspectRatio: 1,
+//       restore: false,
+//       guides: false,
+//       center: false,
+//       highlight: false,
+//       cropBoxMovable: false,
+//       cropBoxResizable: false,
+//       minCropBoxWidth: 280,
+//       minCropBoxHeight: 280,
+//       ready: function(){
+//         croppable = true;
 //       }
-//       reader.readAsDataURL(input.files[0]);
-//     }
+//     });
 //   }
-//   $("#post_image").change(function(){
-//     readURL(this);
-//   });
-//  });
+// });
