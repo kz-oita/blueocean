ea6b0b8c2afc86d53c364f9600bbda69a6881bcf
kz-oita <taoi.kizuka@gmail.com>
2020-05-03 13:26:35 +0900

    投稿数のランキングの実装

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,2 +1,8 @@
 class RankingsController < ApplicationController
+
+
+  def index
+    @post_ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
+    @user_ranks = User.find(Post.group(:user_id).order('count(user_id) desc').limit(3).pluck(:user_id))
+  end
 end
